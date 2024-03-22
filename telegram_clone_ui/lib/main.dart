7a6f2404a7491badc3287a29bba2e81063a85748
emirtascakir <emirtascakir@icloud.com>
2024-03-22 04:00:46 +0300

    telegram clone ui started

---
+++ telegram_clone_ui/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_clone_ui/pages/root_app.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
---
+++ telegram_clone_ui/lib/utlis/constants/constant_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+mixin ConstantColors {
+  static const Color primary = Color(0xFF0088CC);
+  static const Color bgColor = Color(0xFF010101);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color textfieldColor = Color(0xFF1c1d1f);
+  static const Color greyColor = Color(0xFF161616);
+  static const Color chatBoxOther = Color(0xFF3d3d3f);
+  static const Color chatBoxMe = Color(0xFF066162);
+}
